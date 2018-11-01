dc03596ce06fe50622ba3033ce9b28e6d3b85504
Frank Gonzalez <Frank Gonzalez>
2018-11-01 00:58:37 -0400

    conexion2

--- config.js
+++ config.js
@@ -4,14 +4,13 @@ module.exports = {
   SECRET_TOKEN: 'miclavedetokens',
   db: {
     client: 'pg',
-    connection: {
-      /* host     : process.env.DATABASE_URL.host,
+    connection: process.env.DATABASE_URL /* {
+      host     : process.env.DATABASE_URL.host,
       port     : process.env.DATABASE_URL.host.PORT, //5432
       user     : process.env.DATABASE_URL.user,
       password : process.env.DATABASE_URL.password,
       database : process.env.DATABASE_URL.database,
-      charset  : 'utf8' */
-      url      : process.env.DATABASE_URL
-    }
+      charset  : 'utf8' 
+    }*/
   }
 }
