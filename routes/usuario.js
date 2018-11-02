7b43cc8bf1adea960861590fa6cba50bdb188116
Josue <ediksn@gmail.com>
2018-11-02 00:17:58 -0400

    filtro de usuario por correo 2

--- routes/usuario.js
+++ routes/usuario.js
@@ -9,11 +9,11 @@ const controller = require('../controllers/usuario')
 //----Parametros------
 const path = '/usuario'
 const id = ':id'
-
+const correo = ':correo'
 //----Rutas------ 
 router.get(`${path}`, controller.findDocuments)
 router.post(`${path}`,controller.createDocument)
-router.get(`${path}/:correo`,controller.findOneDocument)
+router.get(`${path}/${correo}`,controller.findOneDocument)
 router.put(`${path}/${id}`,controller.updateDocument)
 router.delete(`${path}/${id}`,controller.deleteDocument)
 
