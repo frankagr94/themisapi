900f32cd580f778c8a6369b9941aa9da9d3d0857
Frank Gonzalez <Frank Gonzalez>
2018-11-10 18:48:47 -0400

    arreglo de problemas

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
+const heroku_url =  process.env.DATABASE_URL || 'postgres://kjrfttfynoghsc:74a95068d0d00a672f95e4c084abaac484b39fd8821aa94b13f22682b1fbc7e6@ec2-54-225-98-131.compute-1.amazonaws.com:5432/d9avpgfr1tidjr?ssl=true'
 module.exports = {
   basePath: '/',
   port: process.env.PORT || 3000,
   SECRET_TOKEN: 'miclavedetokens',
   db: {
     client: 'pg',
-    connection: process.env.DATABASE_URL /* {
+    connection: heroku_url /* {
       host     : '127.0.0.1',
       port     : '5432', //5432
       user     : 'postgres',
       password : '1234',
       database : 'themis',
-      charset  : 'utf8' 
+      charset  : 'utf8'
     } */
   }
 }
--- models/inicio_web.js
+++ models/inicio_web.js
@@ -2,11 +2,11 @@
 const Bookshelf = require('../db');
 const Inicio_web = require('./inicio_web');
 
-const Inicio_web = Bookshelf.Model.extend({
+const inicio_web = Bookshelf.Model.extend({
   tableName: 'inicio_web',
   imaganes_carrusel : function(){
     return this.belongsTo(Imagen_carrusel, 'id_inicio_web');
   }
 });
 
-module.exports = Inicio_web;
+module.exports = inicio_web;
