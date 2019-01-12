18d3528f5d5845436c0d3a33ce079e0bae21e984
Josue <ediksn@gmail.com>
2019-01-12 11:58:09 -0400

    modificacion a la actuacion_servicio

--- controllers/servicio.js
+++ controllers/servicio.js
@@ -83,7 +83,7 @@ exports.asociarActuaciones = (req, res)=>{
 
   Servicio.forge(conditions).fetch()
     .then(function(servicio){
-      servicio.actuaciones().attach({actuacion_id:req.body.actuacion_id,estatus:'P', fecha: req.body.fecha, horario_id:req.body.horario_id, abogado_id: req.body.abogado_id})
+      servicio.actuaciones().attach({actuacion_id:req.body.actuacion_id,estatus:'P', fecha_plan: req.body.fecha_plan, horario_id:req.body.horario_id, abogado_id: req.body.abogado_id})
         .then(function(data){
           res.status(200).json({ error: false, data: { message: 'Actuaciones asociadas al servicio' } });
         })
