851cb9186ba378962f2ac887c7d3aafff8257058
Frank Gonzalez <Frank Gonzalez>
2019-01-15 16:38:17 -0400

    guardado de notificaciones en base de datos

--- models/notificacion.js
+++ models/notificacion.js
@@ -1,8 +1,12 @@
 //---- dependencias ------
 const Bookshelf = require('../db');
+const Usuario = require('./usuario');
 
 const Notificacion = Bookshelf.Model.extend({
   tableName: 'notificacion',
+  usuario: function() {
+    return this.belongsTo(Usuario, 'usuario_id');
+  }
 });
 
 module.exports = Notificacion;
