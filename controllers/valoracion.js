68cdd67ad9b94d29251d7e72022b647220cf8638
Josue <ediksn@gmail.com>
2019-01-19 16:14:18 -0400

    filtro a vista valoracion

--- controllers/valoracion.js
+++ controllers/valoracion.js
@@ -47,7 +47,22 @@ exports.findOneValoracion = (req,res) => {
     .catch(function(err){
       res.status(500).json({ error : false, data : {message : err.message} })
     })
+}
 
+exports.findOneValoracionByServicio = (req,res) => {
+
+  let conditions = { servicio_id: req.params.id };
+
+  Valoracion.forge(conditions).fetch()
+    .then(function(data){
+      if(!data) return res.status(404).json({ error : true, data : { message : 'valoracion no existe' } });
+
+      res.status(200).json({ error : false, data : data.toJSON() })
+
+    })
+    .catch(function(err){
+      res.status(500).json({ error : false, data : {message : err.message} })
+    })
 }
 
 exports.updateValoracion = (req,res) => {
