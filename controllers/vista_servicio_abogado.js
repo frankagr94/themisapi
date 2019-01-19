74233830daf4d30b6d243415cf480ed81522e4d6
Josue <ediksn@gmail.com>
2019-01-19 15:55:06 -0400

    filtro a vista de servicios del abogado

--- controllers/vista_servicio_abogado.js
+++ controllers/vista_servicio_abogado.js
@@ -32,9 +32,26 @@ exports.findOneDocument = (req,res) => {
 
 }
 
-exports.findOneDocumentAbogadoId = (req,res) => {
+exports.findOneDocumentAbogadoEstatus = (req,res) => {
 
   let conditions = { abogado_id: req.params.abogado_id, estatus: req.params.estatus };
+
+  Vista_servicio_abogado.where(conditions).fetchAll()
+    .then(function(data){
+      if(!data) return res.status(404).json({ error : true, data : { message : 'vista_servicio_abogado no existe' } });
+
+      res.status(200).json({ error : false, data : data.toJSON() })
+
+    })
+    .catch(function(err){
+      res.status(500).json({ error : false, data : {message : err.message} })
+    })
+
+}
+
+exports.findOneDocumentAbogadoId = (req,res) => {
+
+  let conditions = { abogado_id: req.params.id};
 
   Vista_servicio_abogado.where(conditions).fetchAll()
     .then(function(data){
